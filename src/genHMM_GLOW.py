Some changes and fixes for masking and an actnorm_flag

src/genHMM_GLOW.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import numpy as np
-from gm_hmm.src.glow_model import FlowModel_GLOW
+from gm_hmm.src.glow_model import FlowModel_GLOW, count_params
 import torch
 from torch import nn, distributions
 from gm_hmm.src._torch_hmmc import _compute_log_xi_sum, _forward, _backward
@@ -85,7 +85,7 @@ class GenHMM(torch.nn.Module):
                  net_H=28, net_D=14, K=4, L=2, mask_type="cross", p_drop=0.25,
                  startprob_type="first", transmat_type="random upper triangular",
                  permutation="invconv", coupling="affine", actnorm_scale=1.0,
-                 lu_decomposition=False, sq_factor=1.0, actnorm_flag=True):
+                 lu_decomposition=False, sq_factor=1, actnorm_flag=True):
         
         super(GenHMM, self).__init__()
 
@@ -156,14 +156,13 @@ class GenHMM(torch.nn.Module):
                         self.transmat_[i,j] = 0
 
         elif "ergodic" in transmat_type: 
-            self.transmat_ = torch.ones(self.n_states, self.n_states) \ 
-                             + torch.randn(self.n_states, self.n_states) * 0.01  # Initialises the transition prob. matrix as an ergodic matrix
+            self.transmat_ = torch.ones(self.n_states, self.n_states) + torch.randn(self.n_states, self.n_states) * 0.01  # Initialises the transition prob. matrix as an ergodic matrix
 
         normalize(self.transmat_, axis=1) # Normalizes column-wise so that the transitions of the HMM are valid
         return self
 
     def init_gen(self, n_D, n_H, K, L, permutation='invconv', coupling='affine',
-                 actnorm_scale = 1.0, lu_decomposition = False, sq_factor = 1.0,
+                 actnorm_scale = 1.0, lu_decomposition = False, sq_factor = 1,
                  actnorm_flag = True, p_drop=0):
         
         """
@@ -192,7 +191,7 @@ class GenHMM(torch.nn.Module):
         
         ### torch MultivariateNormal logprob gets error when input is cuda tensor
         ### thus changing it to implementation
-        prior = distributions.MultivariateNormal(torch.zeros(D).to(self.device), torch.eye(D).to(self.device))
+        prior = distributions.MultivariateNormal(torch.zeros(n_D).to(self.device), torch.eye(n_D).to(self.device))
         # prior = lambda x: GaussianDiag.logp(torch.zeros(D), torch.zeros(D), x)
         # self.flow = RealNVP(nets, nett, masks, prior)
 
@@ -200,14 +199,16 @@ class GenHMM(torch.nn.Module):
         self.pi = self.dtype(np.random.rand(self.n_states, self.n_prob_components))
         normalize(self.pi, axis=1)
         self.logPIk_s = self.pi.log()
-
+        n_D = int(n_D)
+        n_H = int(n_H)
         # Init networks
         self.networks = [FlowModel_GLOW(n_D, n_H, K, L, prior, permutation, coupling,\
                                         actnorm_scale, lu_decomposition, sq_factor, \
                                         actnorm_flag, p_drop)
                                         
                                         for _ in range(self.n_prob_components*self.n_states)]
-
+        total_num_params, total_num_trainable_params = count_params(self.networks[0])
+        print("The total number of params: {} and the number of trainable params:{}".format(total_num_params, total_num_trainable_params))
         # Reshape in a n_states x n_prob_components array
         self.networks = np.array(self.networks).reshape(self.n_states, self.n_prob_components)
         
@@ -504,6 +505,7 @@ class GenHMM(torch.nn.Module):
         
         """
         x, x_mask = batch
+        x_mask = x_mask.type(torch.bool)
         batch_size = x.shape[0]
         n_samples = x.shape[1]
 
@@ -546,6 +548,7 @@ class GenHMM(torch.nn.Module):
             # Two posteriors to be computed here:
             # 1. the hidden state posterior, post
             old_llh, old_loglh_sk = self._getllh(self.old_networks, batch)
+            x_mask = x_mask.type(torch.bool)
             old_llh[~x_mask] = 0
             old_logprob, old_fwdlattice = self._do_forward_pass(old_llh, x_mask)
             # assert ((old_logprob <= 0).all())
@@ -556,7 +559,7 @@ class GenHMM(torch.nn.Module):
 
             old_bwdlattice = self._do_backward_pass(old_llh, x_mask)
             posteriors = self._compute_posteriors(old_fwdlattice, old_bwdlattice)
-
+            x_mask = x_mask.type(torch.bool)
             posteriors[~x_mask] = 0
             post = posteriors
 
@@ -569,6 +572,7 @@ class GenHMM(torch.nn.Module):
             
             logpk_sX = log_num - log_denom.reshape(batch_size, n_samples, self.n_states, 1)
             ## To Do: normalize logpk_sX before set un-masked values
+            x_mask = x_mask.type(torch.bool)
             logpk_sX[~x_mask] = 0
         
         # hmm parameters should be updated based on old model
@@ -582,6 +586,7 @@ class GenHMM(torch.nn.Module):
 
         # compute sequence log-likelihood in self.networks, just to monitor the self.networks performance
         with torch.no_grad():
+            x_mask = x_mask.type(torch.bool)
             llh[~x_mask] = 0
             logprob, _ = self._do_forward_pass(llh, x_mask)
         # assert((logprob <= 0).all())
@@ -652,7 +657,7 @@ class GenHMM(torch.nn.Module):
                 self.optimizer.zero_grad()            
                 loss, logprob_ = self.forward(data, testing=False)
                 loss.backward()
-            
+                #print("Loss for EM-Step:{} is {}".format(i, loss))
                 self.optimizer.step()
                 total_loss += loss.detach().data
                 total_logprob += logprob_

src/glow_model.py
@@ -10,9 +10,9 @@ from torch import distributions
 from sklearn import datasets
 import matplotlib.pyplot as plt
 from torch.nn.parameter import Parameter
-from glow_modules import *
-from ops import *
-from plot_functions import plot_results
+from gm_hmm.src.glow_modules import *
+from gm_hmm.src.ops import *
+from gm_hmm.src.plot_functions import plot_results
 from torch.autograd import Function, Variable, detect_anomaly
 from datetime import datetime
 
@@ -73,9 +73,10 @@ class FlowStep(nn.Module):
         assert coupling in self.flow_coupling_list, 'Unsupported flow coupling: {}'.format(coupling)
         self.permutation = permutation
         self.coupling = coupling
-       
+        self.actnorm_flag = actnorm_flag
         # activation normalization layer
-        if actnorm_flag:
+        
+        if self.actnorm_flag:
             self.actnorm = ActNorm(num_channels=in_channels, scale=actnorm_scale)
 
         # flow permutation layer
@@ -108,7 +109,8 @@ class FlowStep(nn.Module):
         """
         #z = copy.deepcopy(x)
         # Activation normalization layer
-        z, logdet = self.actnorm.forward(x, logdet=logdet)
+        if self.actnorm_flag:
+            z, logdet = self.actnorm.forward(x, logdet=logdet)
 
         # Flow permutation layer
         if self.permutation == 'invconv':
@@ -174,7 +176,8 @@ class FlowStep(nn.Module):
             x = self.shuffle.reverse(x)
 
         # activation normalization layer
-        x, logdet = self.actnorm.reverse(x, logdet=logdet)
+        if self.actnorm_flag:
+            x, logdet = self.actnorm.reverse(x, logdet=logdet)
 
         return x, logdet 
 
@@ -261,8 +264,10 @@ class FlowModel_GLOW(nn.Module):
         Defines the function that forms one complete flow through the total network
         in the normalizing direction
         """
+        C = 0
         for layer in self.nets:
             z, logdet = layer.forward(z, logdet)
+            C = C + 1
         return z, logdet
 
     def g(self, z, eps_std=None):
@@ -284,11 +289,15 @@ class FlowModel_GLOW(nn.Module):
         """
         # Ensure that the input has proper dimensions (channel-wise)
         if x.shape[1] != in_channels:
-            X = X.permute(0,2,1)
+            x = x.permute(0,2,1)
         else:
             pass
         z, logp = self.f(x)
         #return self.prior.log_prob(z) + logp
+        if z.shape[1] == in_channels:
+            z = z.permute(0,2,1)
+        else:
+            pass
         px = self.prior.log_prob(z) + logp.view(logp.shape[0], 1)
         if x_mask == None: # x_mask decides if there are appended zeros for the samples which are not to be processed
             return px
@@ -373,7 +382,7 @@ def main():
         pass
     n_HC = 256
     K = 4 # Depth of Flow
-    L = 2 # No. of Layers in Multi-Scale architecture
+    L = 1 # No. of Layers in Multi-Scale architecture
     actnorm_flag = True
     p_drop=0.0 # Dropout Rate
     prior = distributions.MultivariateNormal(torch.zeros(n_IC), torch.eye(n_IC))

src/glow_modules.py
@@ -11,12 +11,12 @@ from math import log, pi, exp
 import numpy as np 
 from scipy import linalg as la
 import datetime as dt
-from ops import *
+from gm_hmm.src.ops import *
 
 # Defines a class for the ActNorm Module
 class ActNorm(nn.Module):
 
-    def __init__(self, num_channels, scale = 1., logscale_factor=3., batch_variance=False):
+    def __init__(self, num_channels, scale = 1., logscale_factor=1e-3, batch_variance=False):
 
         super(ActNorm, self).__init__()
         self.num_channels = num_channels

src/realnvp.py
@@ -60,13 +60,18 @@ class RealNVP(torch.nn.Module):
                      from the latent space Z
              
     """
+    """
+    RealNVP module.
+    Adapted from https://github.com/senya-ashukha/real-nvp-pytorch
+    """
     def __init__(self, nets, mask, prior):
         super(RealNVP, self).__init__()
         
-        self.prior = prior # This initialises the prior distribution (P_z) in the latent space
-        self.mask = torch.nn.Parameter(mask, requires_grad=False) # In the actual NVP paper, masks are used for implementing the mappings
+        self.prior = prior
+        self.mask = torch.nn.Parameter(mask, requires_grad=False)
         self.s = torch.nn.ModuleList([nets() for _ in range(len(mask))])
         self.rescale = torch.nn.utils.weight_norm(Rescale(int(self.mask.size(1)/2)))
+    
     
     def _chunk(self, x, mask):
         """chunk the input x into two chunks along dimension 2
@@ -74,7 +79,8 @@ class RealNVP(torch.nn.Module):
         OUTPUT: tow chunks of tensors with equal size
         """
         idx_id = torch.nonzero(mask).reshape(-1)
-        idx_scale = torch.nonzero(~mask).reshape(-1)
+        idx_scale = (mask == 0).nonzero().reshape(-1)
+        #idx_scale = torch.nonzero(~mask).reshape(-1)
         chunk_id = torch.index_select(x, dim=2,
                                       index=idx_id)
         chunk_scale = torch.index_select(x, dim=2,
@@ -114,7 +120,7 @@ class RealNVP(torch.nn.Module):
         scaling outputs and thus easily computed without derivatives of s or t functions
         """
 
-        log_det_J, z = x.new_zeros((x.shape[0], x.shape[1])), x 
+        log_det_J, z = x.new_zeros((x.shape[0], x.shape[1])), x
         for i in reversed(range(len(self.s))):
             z_id, z_s = self._chunk(z, self.mask[i])
             